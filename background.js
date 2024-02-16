d8dd756fb94ee125246dca889b3654f5314f7535
Benjameslane <ben.james.lane9>
2024-02-16 10:05:00 -0700

    added functionality to our background.js

--- background.js
+++ background.js
@@ -3,4 +3,20 @@
         delayInMinutes: 30,
         periodInMinutes: 30
     });
+
+    chrome.alarms.onAlarm.addListener((alarm) => {
+        if (alarm.name === "breakReminder") {
+          chrome.notifications.create({
+            type: "basic",
+            iconUrl: "images/icon48.png",
+            title: "Time for a Break!",
+            message: "Take a 5-minute break. Click here to start your break timer and play a mini-game.",
+            buttons: [{ title: "Start Break" }]
+          });
+        }
+      });
+      
+
+
+
 }
